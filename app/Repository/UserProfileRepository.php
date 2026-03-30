ad56d1f52805b28bd84c76b10e74f71b18b58b9c
pikio <wibivicky@gmail.com>
2026-03-30 15:09:21 +0700

    update: remove declaration private and change to inside of construct

--- app/Repository/UserProfileRepository.php
+++ app/Repository/UserProfileRepository.php
@@ -3,19 +3,19 @@
 namespace App\Repository;
 
 use App\Models\UserProfile;
-use Illuminate\Support\Facades\Hash;
 
 class UserProfileRepository
 {
-    private $userProfile;
-
-    public function __construct(UserProfile $userProfile) {
-        $this->userProfile = $userProfile;
-    }
+    public function __construct(private UserProfile $userProfile) {}
 
     public function save(UserProfile $userProfile)
     {
         $userProfile->save();
-        return $userProfile;
+        return $userProfile->fresh();
+    }
+
+    public function findByColumn($value, $column)
+    {
+        return $this->userProfile->where($column, $value)->first();
     }
 }
