8edbce7949594ac9cf3de0a6c864543638144853
pikio <wibivicky@gmail.com>
2026-05-12 10:32:47 +0700

    chore: remove unused function

--- app/Http/Controllers/Admin/CategorySkillController.php
+++ app/Http/Controllers/Admin/CategorySkillController.php
@@ -14,34 +14,4 @@ class CategorySkillController
             'categorySkills' => $categorySkills
         ]);
     }
-
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show(string $id)
-    {
-        //
-    }
-
-    public function edit(string $id)
-    {
-        //
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
-    {
-        //
-    }
 }
