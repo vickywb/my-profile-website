3efda22edde6368064f9f3bddf43e9d284fc1d64
pikio <wibivicky@gmail.com>
2026-05-12 16:33:32 +0700

    chore: update and fixing typo

--- app/Service/ExperienceService.php
+++ app/Service/ExperienceService.php
@@ -11,38 +11,32 @@ use App\Repository\ExperienceRepository;
 
 class ExperienceService
 {
-    private $experienceRepository;
+    public function __construct(private ExperienceRepository $experienceRepository) {}
 
-    public function __construct(ExperienceRepository $experienceRepository) {
-        $this->experienceRepository = $experienceRepository;
-    }
-
-    public function handleExperience(array $data): Experience
+    public function handleCreateExperience(array $data): Experience
     {
         try {
             $experience = DB::transaction(function () use ($data) {
                 return $this->experienceRepository->save(new Experience($data));
-
-                Log::info('Experience successfully created.', [
-                    'experience_id' => $experience->id,
-                    'user_id'       => auth()->id(),
-                ]);
-
-                return $experience;
             });
 
-        } catch (\Throwable $th) {
+            Log::info('Experience successfully created.', [
+                'experience_id' => $experience->id,
+                'user_id'       => auth()->id(),
+            ]);
 
-            Log::error('Faild to create data:' . $th->getMessage());
+            return $experience;
+
+        } catch (\Throwable $th) {
+            Log::error('Failed to create data:' . $th->getMessage());
             throw new Exception('Failed to create data:' . $th->getMessage());
         }
     }
 
-    public function handleUpdateExperience(array $data, $experience): Experience
+    public function handleUpdateExperience(array $data, Experience $experience): Experience
     {
         try {
             $experience = DB::transaction(function () use ($data, $experience) {
-
                 $experience->fill($data);
 
                 return $this->experienceRepository->save($experience);
@@ -57,52 +51,49 @@ class ExperienceService
 
         } catch (\Throwable $th) {
 
-            Log::error('Faild to update data:' . $th->getMessage());
+            Log::error('Failed to update data:' . $th->getMessage());
             throw new Exception('Failed to update data:' . $th->getMessage());
         }
     }
 
-    public function handleDeleteExperience($experience): void
+    public function handleDeleteExperience(Experience $experience): void
     {
         try {
-           $experience = DB::transaction(function () use ($experience) {
+            DB::transaction(function () use ($experience) {
                 $experience->delete();
-
-                Log::info('Experience successfully delete.');
-                return $experience;
             });
 
+            Log::info('Experience successfully delete.');
         } catch (\Throwable $th) {
-
-            Log::error('Faild to delete data:' . $th->getMessage());
+            Log::error('Failed to delete data:' . $th->getMessage());
             throw new Exception('Failed to delete data:' . $th->getMessage());
         }
     }
 
-    public function handleUpdateOrCreateJobDescription(array $data, $experience): ExperienceTranslation
+    public function handleUpdateOrCreateJobDescription(array $data, Experience $experience): ExperienceTranslation
     {
         try {
-            $experience = DB::transaction(function () use ($data, $experience) {
-
+            $translation = DB::transaction(function () use ($data, $experience) {
                 return ExperienceTranslation::updateOrCreate(
                     [
                         'experience_id' => $experience->id,
-                        'lang' => $data['lang'],
+                        'lang'          => $data['lang'],
                     ],
                     [
-                        'job_description' =>  $data['job_description']
+                        'job_description' => $data['job_description'],
                     ]
                 );
-                
-                Log::info('Job Description successfully update.');
-                
-                return $experience;
             });
 
-        } catch (\Throwable $th) {
+            Log::info('Job Description successfully updated.', [
+                'experience_id' => $experience->id,
+                'lang'          => $data['lang'],
+            ]);
 
-            Log::error('Faild to update data:' . $th->getMessage());
-            throw new Exception('Failed to update data:' . $th->getMessage());
+            return $translation;
+        } catch (\Throwable $th) {
+            Log::error('Failed to update data: ' . $th->getMessage());
+            throw new Exception('Failed to update data: ' . $th->getMessage()); 
         }
     }
 }
